779e25876d7f4e89c0ab4ff7aece826aec8205e7
Jorge Caraballo Cordovez <jorgecaraballocordovez@MacBook-Pro-de-Jorge.local>
2016-10-21 11:34:11 -0400

    final assignment

--- 6.1/script.js
+++ 6.1/script.js
@@ -27,17 +27,17 @@ d3.csv('../data/olympic_medal_count.csv',parse,function(err, rows){
 		return b.count2012 - a.count2012;
 	})
 
-	var sliceArray = sortArray.slice(0,5);
+	var top5 = sortArray.slice(0,5);
 
 	//set the scales
 
 	var scaleY = d3.scaleLinear()
 		.domain([minY, maxY])
-		.range([h,0]);
+		.range([h, 0]);
 	
 	var scaleX = d3.scaleOrdinal()
-		.domain(sliceArray.map(function(d){ return d.country; }))  //.map creates a new array from an existing array
-		.range(d3.range(0, w, w/5)); // in d3 documentation you need to add a third value where you set what's the distance between the first two (0 and w), that's what w/5 means. 
+		.domain(top5.map(function(d){ return d.country; }))  //.map creates a new array from an existing array
+		.range(d3.range(30, w, w/5)); // in d3 documentation you need to add a third value where you set what's the distance between the first two (0 and w), that's what w/5 means. 
 
 	//draw the Y and X axis
 
@@ -46,18 +46,27 @@ d3.csv('../data/olympic_medal_count.csv',parse,function(err, rows){
 	var axisX = d3.axisBottom()
 		.scale(scaleX);
 
-	plot.append("g").call(axisX);
-	plot.append("g").call(axisY);
+	plot.append("g") //actually appending the axis to the canvas
+		.attr("transform", "translate(0, "+h+")") //moving the x axis to the bottom
+		.attr("class", "axis")
+	.call(axisX);
 
-	//rows.forEach (draw the bars)
+	plot.append("g").attr("class", "axis").call(axisY);
 
-	/* rows.forEach(function(country, i){
-		plot.append("rect")
-		.attr("x",50)
-		.attr("y", function(d){return scaleY;})
-		.attr("width", function(d){return 30;})
-		.attr("height", function(d){return d.count2012;})
-	}); */
+
+	//(draw the bars)
+
+	var rect = plot.selectAll("rect")
+		.data(top5)
+		.enter()
+		.append("rect")
+		.attr("x", function(d){return scaleX(d.country)})
+		.attr("y", function(d){return scaleY(d.count2012)})
+		.attr("width", 30)
+		.attr("height", function(d){return h-scaleY(d.count2012)})
+
+
+	
 
 });
 
